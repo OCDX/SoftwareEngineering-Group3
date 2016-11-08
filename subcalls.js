89533c3642c425bf3a148b455ee0ec064f5c296c
Olivia Apperson <oamr6@mail.missouri.edu>
2016-11-07 18:14:58 -0600

    STUB-CALLS for INTERACTIVE UI ELEMENTS

---
+++ subcalls.js
@@ -0,0 +1,23 @@
+##Stub-calls##
+
+
+####AUTHENTICATION####
+
+function Register(username, email, password) {}
+
+function Login(username, password) {}
+
+function Logout() {} 
+
+
+####FILES####
+
+function UploadFile(title, author, file) {}
+
+function DownloadFile() {}
+
+function EditFile(title, author, file) {}
+
+function DeleteFile(authenticationID, file) {}
+
+function SearchFile(title, author, searchKey) {}
